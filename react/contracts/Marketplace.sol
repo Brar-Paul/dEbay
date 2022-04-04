17e7f4ab15a6be7b8fc1a9a9a0d8b11d7e982a87
Paul Brar <ibrar@ualberta.ca>
2022-04-04 16:42:51 -0600

    modified contract to use wETH

--- react/contracts/Marketplace.sol
+++ react/contracts/Marketplace.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MarketPlace is ReentrancyGuard {
+    IERC20 weth;
     address payable public immutable feeAccount;
     uint256 public immutable feePercent;
     uint256 public listingCount;
@@ -33,43 +35,32 @@ contract MarketPlace is ReentrancyGuard {
 
     mapping(uint256 => Listing) public listings;
 
-    event Listed(
-        uint256 listingId,
-        address indexed nft,
-        uint256 tokenId,
-        uint256 reservcePrice,
-        uint256 startPrice,
-        address indexed seller
-    );
+    event Listed(uint256 indexed listingId, address indexed seller);
 
     event Bid(
-        uint256 listingId,
-        address indexed nft,
-        uint256 tokenId,
+        uint256 indexed listingId,
         uint256 bidPrice,
         address indexed seller,
         address indexed bidder
     );
 
     event Bought(
-        uint256 listingId,
-        address indexed nft,
-        uint256 tokenId,
+        uint256 indexed listingId,
         uint256 salePrice,
         address indexed seller,
         address indexed buyer
     );
 
-    event Closed(uint256 listingId, address seller, address buyer);
-    event confirmedEscrow(uint256 listingId, address seller);
-    event reversedEscrow(uint256 listingId, address seller, address buyer);
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
+    event Closed(uint256 indexed listingId, address seller, address buyer);
+    event confirmedEscrow(uint256 indexed listingId, address seller);
+    event WithdrawEscrow(
+        uint256 indexed listingId,
+        address seller,
+        address buyer
     );
 
-    constructor(uint256 _feePercent) {
+    constructor(uint256 _feePercent, IERC20 _weth) {
+        weth = _weth;
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
     }
@@ -84,8 +75,8 @@ contract MarketPlace is ReentrancyGuard {
     ) external nonReentrant {
         require(_reservePrice >= 0, "Invalid reserve price");
         require(
-            _reservePrice > _startPrice,
-            "Start price must be higher than reserve"
+            _reservePrice >= _startPrice,
+            "Start price cannot be higher than reserve"
         );
         require(
             _closingTime >= (block.timestamp + 1 days),
@@ -104,24 +95,17 @@ contract MarketPlace is ReentrancyGuard {
             AUCTION_STATE.OPEN,
             _closingTime,
             _escrow,
-            payable(0x0000000000000000000000000000000000000000),
-            (_closingTime + (7 * 1 days))
+            payable(address(0)),
+            (_closingTime + (7 days))
         );
-        emit Listed(
-            listingCount,
-            address(_nft),
-            _tokenId,
-            _reservePrice,
-            _startPrice,
-            msg.sender
-        );
+        emit Listed(listingCount, msg.sender);
     }
 
     function bid(uint256 _listingId, uint256 _bidPrice) external nonReentrant {
         Listing memory listing = listings[_listingId];
         address payable bidder = payable(msg.sender);
         require(
-            bidder.balance > listing.currentPrice,
+            weth.balanceOf(bidder) > listing.currentPrice,
             "You don't have enough ETH to cover this bid!"
         );
         require(
@@ -132,19 +116,13 @@ contract MarketPlace is ReentrancyGuard {
             listing.auctionState == AUCTION_STATE.OPEN,
             "This item is closed for bidding"
         );
+        weth.approve(address(this), _bidPrice);
         listing.currentPrice = _bidPrice;
         listing.buyer = bidder;
-        emit Bid(
-            _listingId,
-            address(listing.nft),
-            listing.tokenId,
-            _bidPrice,
-            listing.seller,
-            bidder
-        );
+        emit Bid(_listingId, _bidPrice, listing.seller, bidder);
     }
 
-    function endAuction(uint256 _listingId) external payable nonReentrant {
+    function endAuction(uint256 _listingId) external nonReentrant {
         Listing memory listing = listings[_listingId];
         require(
             msg.sender == listing.seller || msg.sender == listing.buyer,
@@ -154,68 +132,93 @@ contract MarketPlace is ReentrancyGuard {
             block.timestamp > listing.closingTime,
             "Auction is still running"
         );
-        require(
-            msg.value == listing.currentPrice,
-            "Not enough ETH to cover price!"
-        );
-        // Update auction state if transaction fails
-        if (msg.value < listing.currentPrice) {
+        // Cancels deal if buyer does not have balance to cover sale price
+        if (weth.balanceOf(listing.buyer) < listing.currentPrice) {
+            revert(
+                "Buyer does not wETH balance to cover the sale price, sale reverted"
+            );
             listing.auctionState = AUCTION_STATE.CLOSED;
             emit Closed(_listingId, listing.seller, listing.buyer);
-            // Ban buyer address from bidding ?
         }
+        // Auto-Finalize and transfer if no escrow
         if (!listing.escrow) {
-            // Auto-Finalize and transfer if no escrow
             listing.auctionState == AUCTION_STATE.FINALIZED;
-            listing.seller.transfer(listing.currentPrice * (1 - feePercent));
-            feeAccount.transfer(listing.currentPrice * feePercent);
-            listing.nft.transferFrom(
+            weth.transferFrom(
+                listing.buyer,
+                listing.seller,
+                listing.currentPrice * (1 - feePercent)
+            );
+            weth.transferFrom(
+                listing.buyer,
+                feeAccount,
+                listing.currentPrice * feePercent
+            );
+            listing.nft.safeTransferFrom(
                 address(this),
                 listing.buyer,
                 listing.tokenId
             );
             emit Bought(
                 _listingId,
-                address(listing.nft),
-                listing.tokenId,
                 listing.currentPrice,
                 listing.seller,
                 listing.buyer
             );
         } else {
+            weth.transferFrom(
+                listing.buyer,
+                address(this),
+                listing.currentPrice
+            );
             listing.auctionState == AUCTION_STATE.PENDING;
         }
     }
 
     function confirmEscrow(uint256 _listingId) external payable nonReentrant {
         Listing memory listing = listings[_listingId];
-        require(msg.sender == listing.buyer, "Only buyer can alter escrow");
+        require(msg.sender == listing.buyer, "Only buyer can finalize escrow");
         require(listing.auctionState == AUCTION_STATE.PENDING);
         require(
             block.timestamp < listing.escrowClosingTime,
             "Escrow has expired, the sale will be reversed"
         );
         listing.seller.transfer(listing.currentPrice * (1 - feePercent));
-        listing.nft.transferFrom(address(this), listing.buyer, listing.tokenId);
+        weth.transferFrom(
+            address(this),
+            feeAccount,
+            listing.currentPrice * feePercent
+        );
+        listing.nft.safeTransferFrom(
+            address(this),
+            listing.buyer,
+            listing.tokenId
+        );
         listing.auctionState = AUCTION_STATE.FINALIZED;
         emit confirmedEscrow(_listingId, listing.seller);
     }
 
-    function reverseEscrow(uint256 _listingId) external payable nonReentrant {
+    function withdrawEscrow(uint256 _listingId) external payable nonReentrant {
         Listing memory listing = listings[_listingId];
-        require(msg.sender == listing.buyer, "Only buyer can alter escrow");
+        require(
+            msg.sender == listing.buyer,
+            "Only buyer can withdraw from escrow"
+        );
         require(listing.auctionState == AUCTION_STATE.PENDING);
         require(
             block.timestamp > listing.escrowClosingTime,
             "The escrow time has not expired yet"
         );
-        listing.buyer.transfer(listing.currentPrice * (1 - feePercent));
-        listing.nft.transferFrom(
+        weth.transferFrom(
+            address(this),
+            listing.buyer,
+            listing.currentPrice * (1 - feePercent)
+        );
+        listing.nft.safeTransferFrom(
             address(this),
             listing.seller,
             listing.tokenId
         );
         listing.auctionState = AUCTION_STATE.FINALIZED;
-        emit reversedEscrow(_listingId, listing.seller, listing.buyer);
+        emit WithdrawEscrow(_listingId, listing.seller, listing.buyer);
     }
 }
