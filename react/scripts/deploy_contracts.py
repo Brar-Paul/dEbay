258da65c8a81dc78dfd8c72c3636f018f1d818f6
Paul Brar <ibrar@ualberta.ca>
2022-04-05 18:36:30 -0600

    Deploy script

---
+++ react/scripts/deploy_contracts.py
@@ -0,0 +1,18 @@
+from brownie import Marketplace, NFT
+
+
+def deployMarketplace():
+    pass
+
+
+def deployNFT():
+    pass
+
+
+def getAccount():
+    pass
+
+
+def main():
+    deployNFT()
+    deployMarketplace()
