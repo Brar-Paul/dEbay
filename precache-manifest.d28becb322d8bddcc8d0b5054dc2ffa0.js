6ff98f7391e63ce4a9ca447cdbafe29684223a9b
Paul Brar <ibrar@ualberta.ca>
2022-04-29 14:48:01 -0600

    Updates

---
+++ precache-manifest.d28becb322d8bddcc8d0b5054dc2ffa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44889b8e9120ce74e1b6cc79b5b944e",
+    "url": "/dEbay/index.html"
+  },
+  {
+    "revision": "4ca3bacefb599f28d562",
+    "url": "/dEbay/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "b53e7585cd18dbec1db9",
+    "url": "/dEbay/static/css/main.23897544.chunk.css"
+  },
+  {
+    "revision": "4ca3bacefb599f28d562",
+    "url": "/dEbay/static/js/2.1790b688.chunk.js"
+  },
+  {
+    "revision": "25d4e673662a957c20aa4b3c4ab19362",
+    "url": "/dEbay/static/js/2.1790b688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53e7585cd18dbec1db9",
+    "url": "/dEbay/static/js/main.77b5da3d.chunk.js"
+  },
+  {
+    "revision": "b3cc2da67e587a184a8f",
+    "url": "/dEbay/static/js/runtime-main.67942050.js"
+  }
+]);
